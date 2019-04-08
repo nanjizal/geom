1caf9a954f08d268d560192c02f4b985427949c8
nanjizal <nanjizal@users.noreply.github.com>
2019-04-08 11:51:00 +0100

    Update Tri3D.hx

--- src/geom/Tri3D.hx
+++ src/geom/Tri3D.hx
@@ -24,12 +24,12 @@ abstract Tri3D( TriVec ) to TriVec from TriVec {
     }
     @:op(A + B)
     public static inline
-    function add( tv: TriVec, p: Matrix1x4 ){
+    function add0( tv: TriVec, p: Matrix1x4 ){
         return new Tri3D(tv.a + p, tv.b + p, tv.c + p );
     }
     @:op(A + B)
     public static inline
-    function add( p: Matrix1x4, tv: TriVec ){
+    function add1( p: Matrix1x4, tv: TriVec ){
         return new Tri3D(tv.a + p, tv.b + p, tv.c + p );
     }
     @:op(A - B)
