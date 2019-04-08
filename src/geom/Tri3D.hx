bd7003c8e7fb03c72e04804a123564214d004b9c
nanjizal <nanjizal@users.noreply.github.com>
2019-04-08 10:55:16 +0100

    Update Tri3D.hx

--- src/geom/Tri3D.hx
+++ src/geom/Tri3D.hx
@@ -38,4 +38,28 @@ abstract Tri3D( TriVec ) to TriVec from TriVec {
     function scaleMultiply( tv: TriVec, p: Matrix1x4 ){
         return new Tri3D(tv.a * p, tv.b * p, tv.c * p );    
     }
+    public inline 
+    function centre(): Matrix1x4 {
+        return ( this.a + this.b + this.c )/3;
+    }
+    // Quaterion rotation
+    // maybe better approach
+    public inline
+    function multiplyQ( q: Matrix1x4 ):Matrix1x4 {
+        var cp = centre();
+        var a2 = this.a - cp;
+        var b2 = this.b - cp;
+        var c2 = this.c - cp;
+        var aout = Matrix1x4.unit();
+        var bout = Matrix1x4.unit();
+        var cout = Matrix1x4.unit();
+        Matrix1x4.multiplyQ( q, a2, aout ); 
+        Matrix1x4.multiplyQ( q, b2, bout );
+        Matrix1x4.multiplyQ( q, c2, cout );
+        a2 = aout + cp;
+        b2 = bout + cp;
+        c2 = bout + cp;
+        this = new Tri3D( { a: a2, b: b2, c: c2 } );
+        return this;
+    }
 }
