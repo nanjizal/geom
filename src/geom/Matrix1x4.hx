984b63263c47293cfa03cb223646343c68968176
nanjizal <nanjizal@users.noreply.github.com>
2019-04-10 15:25:47 +0100

    Update Matrix1x4.hx

--- src/geom/Matrix1x4.hx
+++ src/geom/Matrix1x4.hx
@@ -155,10 +155,10 @@ abstract Matrix1x4( Tmatrix1x4 ) to Tmatrix1x4 from Tmatrix1x4 {
     }
     public inline 
     function cross(v: Matrix1x4 ): Matrix1x4 {
-		return new Vector3( { x: this.y * v.z - this.z * v.y
-		                    , y: this.z * v.x - this.x * v.z
-		                    , z: this.x * v.y - this.y * v.x
-                                    , w: this.w * v.w } );
+		return new Matrix1x4( { x: this.y * v.z - this.z * v.y
+		                      , y: this.z * v.x - this.x * v.z
+		                      , z: this.x * v.y - this.y * v.x
+                                      , w: this.w * v.w } );
     }
     public static inline
     function fromAxisAngle( theta: Float, axis: Matrix1x4 ):Matrix1x4 {
