77c7d4936f0f2154b463220e75776ea34262fbdc
nanjizal <none>
2019-06-25 17:41:05 +0100

    add ability to increment and decrement through Trinary states

--- src/geom/Trinary.hx
+++ src/geom/Trinary.hx
@@ -26,4 +26,30 @@ class Trinary{
     function get_trit(): Trit {
         return trit;
     }
+    /**
+     * allows you to toggle forward in states.
+     **/
+    function increment(){
+        switch( trit ){
+            case negative:
+                trit = zero;
+            case zero:
+                trit = positive;
+            case positive:
+                trit = negative;
+        }
+    }
+    /**
+     * allows you to toggle backwards in states.
+     **/
+    function decrement(){
+        switch( trit ){
+            case negative:
+                trit = positive;
+            case zero:
+                trit = negative;
+            case positive:
+                trit = zero;
+        }
+    }
 }
