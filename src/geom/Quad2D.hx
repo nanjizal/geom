19a7ec2b4e1f28b181c7c5a911ae952a4d747667
nanjizal <nanjizal@users.noreply.github.com>
2019-12-29 18:27:43 +0000

    Update Quad2D.hx

--- src/geom/Quad2D.hx
+++ src/geom/Quad2D.hx
@@ -5,7 +5,7 @@ class Quad2D {
     public var A: Matrix1x2; 
     public var B: Matrix1x2;
     public var C: Matrix1x2;
-    public var D: Matrix1x2
+    public var D: Matrix1x2;
     function new( A_: Matrix1x2, B_: Matrix1x2, C_: Matrix1x2, D_: Matrix1x2 ){
         A = A_; 
         B = B_;
