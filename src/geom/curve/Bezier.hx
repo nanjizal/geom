5392996ec59a9f5e704a61b171d7630cece5cc79
nanjizal <nanjizal@users.noreply.github.com>
2019-12-31 05:31:00 +0000

    add subdiv and map to basic as single method.

--- src/geom/curve/Bezier.hx
+++ src/geom/curve/Bezier.hx
@@ -16,24 +16,52 @@ class Bezier {
         var b = 0.39;
         return x * (1 - b + Math.sqrt(b * b + 0.25 * x * x));
     }
-    // The maximum error between a chord and the quadratic BÃ©zier.
+    // The maximum error between a chord and the quadratic Bezier.
     // Note: this isn't quite right for extreme examples.
     public static inline
     function quadError( p0: Matrix1x2, p1: Matrix1x2, p2: Matrix1x2 ): Float {
-        // clone points first?
-        var p1_ = p1 - p0;
-        var p2_ = p2 - p0;
-        var t = ( p1_.x * p2_.x + p1_.y * p2_.y) / ( p2_.x * p2_.x + p2_.y * p2_.y );
-        var u = Interval.unitFloat( t );
-        var p = p0.lerp( p2_, u );
-        return 0.5 * p.distanceBetweenSquare_(p1_);///distanceBetweenSquare_
+        var p10 = p1 - p0;
+        var p20 = p2 - p0;
+        var t = ( p10 * p20 ) / ( p20 * p20 );
+        var p = p0.lerpClamp( p20, t );
+        return 0.5 * p.distanceBetweenSquare_( p10 );
+    }
+    // Subdivide using fancy algorithm.
+    public static inline 
+    function subdiv( tol: Float, p0: Matrix1x2, p1: Matrix1x2, p2: Matrix1x2 ){
+        // Determine the x values and scaling to map to y=x^2
+        // map_to_basic
+        var dd = 2*p1 - p0 - p2;
+        var p10 = p1 - p0;
+        var p21 = p2 - p1;
+        var p20 = p2 - p0;
+        var u0 = p10 * dd;
+        var u2 = p20 * dd;
+        var cross = p20 * dd;
+        var x0 = u0 / cross;
+        var x2 = u2 / cross;
+        // There's probably a more elegant formulation of this...
+        var scale = Math.abs( cross ) / ( dd.magnitude * Math.abs( x2 - x0 ) );
+        var a0 = approx_myint( x0 );
+        var a2 = approx_myint( x2 );
+        var count =  0.5 * Math.abs( a2 - a0 ) * Math.sqrt( params.scale / tol );
+        var n = Math.ceil( count );
+        var u0 = approx_inv_myint( a0 );
+        var u2 = approx_inv_myint( a2 );
+        var result = [ 0. ];
+        for( i in 1...n ){
+            var u = approx_inv_myint( a0 + ((a2 - a0) * i) / n );
+            var t = (u - u0) / (u2 - u0);
+            result[ i ] = t;
+        }
+        result[ n ] = 1.;
+        return result;
     }
     public static inline
     function quadratic ( t: Float, s: Float, c: Float, e: Float ): Float {
         var u = 1 - t;
         return Math.pow( u, 2 )*s + 2*u*t*c + Math.pow( t, 2 )*e;
     }
-    
     public static inline
     function quadSegment( t0: Float, t1: Float, s: Float, c: Float, e: Float ){
         var u = t1 - t0;
