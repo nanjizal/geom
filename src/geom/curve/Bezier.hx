91daa6030b72d0a4691c63615822b9d92aee2b09
nanjizal <nanjizal@users.noreply.github.com>
2019-12-31 08:31:58 +0000

    fixed some compile errors.

--- src/geom/curve/Bezier.hx
+++ src/geom/curve/Bezier.hx
@@ -1,6 +1,5 @@
 package geom.curve;
-// considering a Raph tweet on Beziers on segmentation, porting and distorting from js slowly.
-// currently only quadratic and cubic used.
+
 class Bezier {
     // Compute an approximation to int (1 + 4x^2) ^ -0.25 dx
     // This isn't especially good but will do.
@@ -20,44 +19,62 @@ class Bezier {
     // Note: this isn't quite right for extreme examples.
     public static inline
     function quadError( p0: Matrix1x2, p1: Matrix1x2, p2: Matrix1x2 ): Float {
-        var p10 = p1 - p0;
-        var p20 = p2 - p0;
-        var t = ( p10 * p20 ) / ( p20 * p20 );
+        var p10: Matrix1x2 = p1 - p0;
+        var p20: Matrix1x2 = p2 - p0;
+        var t = ( p10 * p20 ).sumXY() / ( p20 * p20 ).sumXY();
         var p = p0.lerpClampT( p20, t );
-        return 0.5 * p.distanceBetweenSquare_( p10 );
+        return 0.5 * p.distanceBetweenSquare_( p10 );///distanceBetweenSquare_
     }
     // Subdivide using fancy algorithm.
     public static inline 
     function subdiv( tol: Float, p0: Matrix1x2, p1: Matrix1x2, p2: Matrix1x2 ){
         // Determine the x values and scaling to map to y=x^2
-        // map_to_basic
+        // map_to_basic() {
         var dd = 2*p1 - p0 - p2;
         var p10 = p1 - p0;
         var p21 = p2 - p1;
         var p20 = p2 - p0;
-        var u0 = p10 * dd;
-        var u2 = p20 * dd;
-        var cross = p20 * dd;
+        var u0 = ( p10 * dd ).sumXY();
+        var u2 = ( p20 * dd ).sumXY();
+        var cross: Float = p20.perp( dd );
         var x0 = u0 / cross;
         var x2 = u2 / cross;
         // There's probably a more elegant formulation of this...
-        var scale = Math.abs( cross ) / ( dd.magnitude * Math.abs( x2 - x0 ) );
+        var scale: Float = Math.abs( cross ) / ( dd.magnitude * Math.abs( x2 - x0 ) );
         // subdivide
-        var a0 = approx_myint( x0 );
-        var a2 = approx_myint( x2 );
-        var count =  0.5 * Math.abs( a2 - a0 ) * Math.sqrt( params.scale / tol );
+        var a0 = approxInterval( x0 );
+        var a2 = approxInterval( x2 );
+        var count =  0.5 * Math.abs( a2 - a0 ) * Math.sqrt( scale / tol );
         var n = Math.ceil( count );
-        var u0 = approx_inv_myint( a0 );
-        var u2 = approx_inv_myint( a2 );
+        var u0 = approxInverseInterval( a0 );
+        var u2 = approxInverseInterval( a2 );
         var result = [ 0. ];
         for( i in 1...n ){
-            var u = approx_inv_myint( a0 + ((a2 - a0) * i) / n );
+            var u = approxInverseInterval( a0 + ((a2 - a0) * i) / n );
             var t = (u - u0) / (u2 - u0);
             result[ i ] = t;
         }
         result[ n ] = 1.;
         return result;
     }
+    // Subdivide using method from Sederberg's CAGD notes
+    /*
+    public static inline
+    function sederberg( tol: Float , roundToPow2, p0: Matrix1x2, p1: Matrix1x2, p2: Matrix1x2 ){
+        var dd = 2 * p1 - p0 - p2;
+        dd = dd.magnitude;
+        var n = Math.ceil( Math.sqrt(0.25 * dd / tol ) );
+        if( roundToPow2 ) {
+            n = 1 << 32 - Math.clz32(n - 1);
+        }
+        let result = [];
+        for (let i = 0; i < n; i++) {
+            const t = i / n;
+            result.push(t);
+        }
+        result.push(1);
+        return result;
+    }*/
     public static inline
     function quadratic ( t: Float, s: Float, c: Float, e: Float ): Float {
         var u = 1 - t;
