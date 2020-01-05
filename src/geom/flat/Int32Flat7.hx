1f64ab2bcd6780e4fe431b25848be96dea8b8e6a
nanjizal <nanjizal@users.noreply.github.com>
2020-01-05 16:55:58 +0000

    Create Int32Flat7.hx

---
+++ src/geom/flat/Int32Flat7.hx
@@ -0,0 +1,32 @@
+package geom.flat;
+import haxe.io.Int32Array; 
+@:forward
+abstract Int32Flat7( Int32Flat ) {
+    @:op([]) public inline 
+    function readItem( k: Int ): Int {
+      return this.readItem( pos*7 + k );
+    }
+    @:op([]) public inline 
+    function writeItem( k: Int, v: Int ): Int {
+        this.writeItem( pos*7 + k, v );
+        return v;
+    }
+    public inline 
+    function new( len: Int ){
+        this = new Int32Flat( len );
+    }
+    public var pos( get, set ): Int;
+    inline
+    function get_pos(): Int {
+        return this.pos;
+    }
+    inline
+    function set_pos( id: Int ): Int {
+        this.pos = id;
+        return id;
+    }
+    public inline
+    function getArray(): Int32Array {
+        return this.subarray( 2, this.length*7 + 2 );
+    }
+}
