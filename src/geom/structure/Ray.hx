89302bce6036062342161857e355b0d5ec9d7762
nanjizal <nanjizal@users.noreply.github.com>
2020-01-05 14:44:57 +0000

    Create Ray.hx

---
+++ src/geom/structure/Ray.hx
@@ -0,0 +1,18 @@
+package geom.structure;
+import geom.matrix.Matrix1x4;
+/** 
+   { dist, edge, point, norm }
+**/
+@:structInit
+class Ray {
+    public var dist = 0.; 
+    public var edge = 0.;
+    public var norm: Matrix1x4;
+    public var refl:  Matrix1x4;
+    function new( dist: Float, edge: Float, norm: Matrix1x4, refl: Matrix1x4 ){
+        this.dist  = dist; 
+        this.edge  = edge;
+        this.norm = norm;
+        this.refl  = refl;
+    }
+}
