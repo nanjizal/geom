191d407eea6f55d491b4a79dffbebd880716882d
nanjizal <nanjizal@users.noreply.github.com>
2020-01-05 17:16:53 +0000

    Create FloatFlat5.hx

---
+++ src/geom/flat/FloatFlat5.hx
@@ -0,0 +1,31 @@
+package flat;
+@:forward
+abstract FloatFlat5( FloatFlat ) {
+    @:op([]) public inline 
+    function readItem( k: Int ): Float {
+      return  this.readItem( index*5 + k );
+    }
+    @:op([]) public inline 
+    function writeItem( k: Int, v: Float ): Float {
+        this.writeItem( index*5 + k, v );
+        return v;
+    }
+    public inline 
+    function new( len: Int ){
+        this = new Array<Float>();
+    }
+    public var index( get, set ): Int;
+    inline
+    function get_index(): Int {
+        return this.index;
+    }
+    inline
+    function set_index( id: Int ): Int {
+        this.index = id;
+        return id;
+    }
+    public inline
+    function getArray(): Array<Float> {
+        return this.slice( 2, this.length*5 + 2 );
+    }
+}
