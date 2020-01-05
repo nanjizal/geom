d84af0f5131bb8f381a09e595ec3f3cecaf63b1c
nanjizal <nanjizal@users.noreply.github.com>
2020-01-05 18:13:08 +0000

    Update IntFlatTriangle.hx

--- src/geom/flat/IntFlatTriangle.hx
+++ src/geom/flat/IntFlatTriangle.hx
@@ -262,7 +262,7 @@ abstract IntFlatTriangle( IntFlat9 ){
     public inline
     function prettyAll(){
         this.pos = 0;
-        var str = 'FloatFlatTriangle: \n';
+        var str = 'IntFlatTriangle: \n';
         for( i in 0...this.length ) {
             str += prettyString();
             this.next();
