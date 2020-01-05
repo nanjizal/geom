b889930599bdd5f336d49db5434f7b73fca50e84
nanjizal <nanjizal@users.noreply.github.com>
2020-01-05 17:12:17 +0000

    Create FlatFloat2.hx

---
+++ src/geom/flat/FlatFloat2.hx
@@ -0,0 +1,33 @@
+  
+package geom.flat;
+import haxe.io.Float32Array; 
+@:forward
+abstract Float32Flat2( Float32Flat ) {
+    @:op([]) public inline 
+    function readItem( k: Int ): Float {
+      return  this.readItem( index*2 + k );
+    }
+    @:op([]) public inline 
+    function writeItem( k: Int, v: Float ): Float {
+        this.writeItem( index*2 + k, v );
+        return v;
+    }
+    public inline 
+    function new( len: Int ){
+        this = new Float32Flat( len );
+    }
+    public var index( get, set ): Int;
+    inline
+    function get_index(): Int {
+        return this.index;
+    }
+    inline
+    function set_index( id: Int ): Int {
+        this.index = id;
+        return id;
+    }
+    public inline
+    function getArray(): Float32Array {
+        return this.subarray( 2, this.length*2 + 2 );
+    }
+}
