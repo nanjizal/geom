d32136c555dc7d59b0395a13f198118bf86bbe84
nanjizal <nanjizal@users.noreply.github.com>
2020-01-05 17:02:40 +0000

    Create UInt16Flat2.hx

---
+++ src/geom/flat/UInt16Flat2.hx
@@ -0,0 +1,32 @@
+package geom.flat;
+import haxe.io.Int16Array; 
+@:forward
+abstract UInt16Flat2( UInt16Flat ) {
+    @:op([]) public inline 
+    function readItem( k: Int ): UInt {
+      return this.readItem( pos*2 + k );
+    }
+    @:op([]) public inline 
+    function writeItem( k: Int, v: UInt ): UInt {
+        this.writeItem( pos*2 + k, v );
+        return v;
+    }
+    public inline 
+    function new( len: Int ){
+        this = new UInt16Flat( len );
+    }
+    public var pos( get, set ): UInt;
+    inline
+    function get_pos(): UInt {
+        return this.pos;
+    }
+    inline
+    function set_pos( id: UInt ): UInt {
+        this.pos = id;
+        return id;
+    }
+    public inline
+    function getArray(): UInt16Array {
+        return this.subarray( 2, this.length*2 + 2 );
+    }
+}
