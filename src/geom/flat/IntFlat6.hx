c3b20d6e8ca07a163c08db6441c3fd0d8d726b5b
nanjizal <nanjizal@users.noreply.github.com>
2020-01-05 18:02:55 +0000

    Create IntFlat6.hx

---
+++ src/geom/flat/IntFlat6.hx
@@ -0,0 +1,31 @@
+package geom.flat; 
+@:forward
+abstract IntFlat6( IntFlat ) {
+    @:op([]) public inline 
+    function readItem( k: Int ): Int {
+      return this.readItem( pos*6 + k );
+    }
+    @:op([]) public inline 
+    function writeItem( k: Int, v: Int ): Int {
+        this.writeItem( pos*6 + k, v );
+        return v;
+    }
+    public inline 
+    function new( len: Int ){
+        this = new IntFlat( len );
+    }
+    public var pos( get, set ): Int;
+    inline
+    function get_pos(): Int {
+        return this.pos;
+    }
+    inline
+    function set_pos( id: Int ): Int {
+        this.pos = id;
+        return id;
+    }
+    public inline
+    function getArray(): Array<Int> {
+        return this.slice( 2, this.length*6 + 2 );
+    }
+}
