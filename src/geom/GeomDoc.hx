1a46ae8d3a273fa9069353be24a57d7df5aecfde
nanjizal <none>
2020-01-07 03:49:12 +0000

    _

--- src/geom/GeomDoc.hx
+++ src/geom/GeomDoc.hx
@@ -1,6 +1,15 @@
 package geom;
 import geom.*;
+import geom.constraints.*;
+import geom.curve.*;
+import geom.flat.*;
 import geom.matrix.*;
+import geom.move.*;
+import geom.obj.*;
+import geom.structure.*;
+import geom.systemL.*;
+import geom.tydef.*;
+
 @:dox(hide)
 class GeomDoc {
     public static function main(){
