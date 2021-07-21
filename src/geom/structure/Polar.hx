022c4a87099128e5efe1b3eefc932717bdcff4a4
nanjizal <nanjizal@users.noreply.github.com>
2021-07-21 11:04:59 +0100

    Update Polar.hx

--- src/geom/structure/Polar.hx
+++ src/geom/structure/Polar.hx
@@ -1,1 +1,20 @@
-..
+package geom.structure;
+/** 
+   { phase, magnitude }
+**/
+@:structInit
+class Polar {
+    public var phase = 0.; 
+    public var magnitude = 0.;
+    function new( phase: Float, magnitude: Float ){
+        this.phase = phase; 
+        this.magnitude = magnitude;
+    }
+    // for rough debug, perhaps remove later.
+    public function values(){
+        return phase + ' ' + magnitude;
+    }
+    public function clone(): Polar {
+        return { phase: this.phase, magnitude: this.magnitude };
+    }
+}
